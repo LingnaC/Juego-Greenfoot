48705053d2afd0a708202899ff63f2f5e1e62423
LingnaC <131807587+LingnaC@users.noreply.github.com>
2023-05-27 15:56:59 -0600

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+
+/**
+ * Write a description of class Counter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Counter extends Actor
+{
+    private int score;
+    
+    public Counter()
+    {
+        score = 0;
+        setImage (new GreenfootImage(200, 30));
+        update();
+    }
+    
+    public void addScore()
+    {
+        score++;
+        update();
+    }
+    
+    private void update()
+    {
+        GreenfootImage img = getImage();
+        img.clear();
+        img.setColor(Color.BLACK);
+        img.setFont(img.getFont().deriveFont(16.0F));
+        img.drawString("Score: " + score, 4, 20);
+    }
+}
